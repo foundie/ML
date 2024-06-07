b84f204053b3ca818e28a5d99d6afe774a9bb0e0
Erlis Rizqiyani <101242934+erlisrizqiyani@users.noreply.github.com>
2024-06-07 14:19:23 +0700

    color analysis

--- Color Analysis/color_season.py
+++ Color Analysis/color_season.py
@@ -18,37 +18,16 @@ if 'season' not in df.columns:
 # Define brightness factors for new chroma choices
 brightness_factors = [0.8, 0.9, 1.1, 1.2]
 
-# Dictionary to store dataframes for each season
-season_dfs = {
-    'spring_light': pd.DataFrame(),
-    'spring_clear': pd.DataFrame(),
-    'spring_warm': pd.DataFrame(),
-    'summer_light': pd.DataFrame(),
-    'summer_soft': pd.DataFrame(),
-    'summer_cool': pd.DataFrame(),
-    'autumn_deep': pd.DataFrame(),
-    'autumn_soft': pd.DataFrame(),
-    'autumn_warm': pd.DataFrame(),
-    'winter_deep': pd.DataFrame(),
-    'winter_clear': pd.DataFrame(),
-    'winter_cool': pd.DataFrame(),
-}
-
 # Process each row in the dataframe
-for index, row in df.iterrows():
-    season = row['season'].replace(' ', '_').lower()
-    original_rgb = np.array([row['R'], row['G'], row['B']])
+adjusted_dfs = []
 
-    for factor in brightness_factors:
-        new_rgb = adjust_brightness(original_rgb, factor)
-        new_row = {
-            'season': row['season'],
-            'R': new_rgb[0],
-            'G': new_rgb[1],
-            'B': new_rgb[2]
-        }
-        season_dfs[season] = season_dfs[season]._append(new_row, ignore_index=True)
+for factor in brightness_factors:
+    adjusted_df = df.copy()  # Create a copy of the original dataframe
+    adjusted_df[['R_adjusted', 'G_adjusted', 'B_adjusted']] = np.clip(df[['R', 'G', 'B']] * factor, 0, 255).astype(int)
+    adjusted_dfs.append(adjusted_df)
 
-# Save each season dataframe to a separate CSV file
-for season, season_df in season_dfs.items():
-    season_df.to_csv(f'd:\!KULIAH!\Semester 6\Bangkit Course\Belajar Analisi Data dengan Python\Submission\ML\Color Analysis\{season}_adjusted_colors.csv', index=False)
+# Concatenate all adjusted dataframes
+df_adjusted = pd.concat(adjusted_dfs, ignore_index=True)
+
+# Save the combined dataframe to a CSV file
+df_adjusted.to_csv('D:\!KULIAH!\Semester 6\Bangkit Course\Belajar Analisi Data dengan Python\Submission\ML\Color Analysis\personal_color_season_adjusted.csv', index=False)
